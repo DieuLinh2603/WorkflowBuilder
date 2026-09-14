e2ef09a14aa5ba1ef4c1919d880cdb05e0fca93d
KhangNguyen0105 <khangnguyen.010598@gmail.com>
2026-09-14 22:30:34 +0700

    chore: untrack backend/target build artifacts from git

--- backend/target/classes/db/migration/V4__advanced_condition_expressions.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE workflow_condition_clauses ADD COLUMN expression_json TEXT;
-ALTER TABLE workflow_condition_clauses ALTER COLUMN field_key DROP NOT NULL;
-ALTER TABLE workflow_condition_clauses ALTER COLUMN operator DROP NOT NULL;
-ALTER TABLE workflow_connections ADD COLUMN priority INTEGER NOT NULL DEFAULT 100;
